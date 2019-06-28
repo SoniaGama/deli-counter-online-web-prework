2c6d2c63364ba635de05b39ffd67f15474aa47ae
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-28 04:19:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(array)
+  line_current = "The line is currently: "
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    array.each_with_index{|names, index| line_current.push("#{index}. "{names})}
+    puts line_current
+  end
+#The line is currently empty.
+#The line is currently: 1. Logan 2. Avi 3. Spencer
+#.each_with_index  
+end
+
+def take_a_number(array, line_current)
+end
+

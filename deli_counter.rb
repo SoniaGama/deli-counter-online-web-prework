270c402c86784f82ade552a7284711521ada5974
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-28 04:25:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,6 +9,12 @@ def line(array)
 end
 
 def take_a_number(array, final_name)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    array.each_with_index{|names, index| line_current.push("#{index + 1}. #{names}")}
+    puts line_current
+  end
   
   El método debe llamar (poner) el nombre de la persona junto con su posición en línea. 
 Sugerencia: recuerde que a las personas les gusta contar desde 1, no desde 0 ("cero") 
